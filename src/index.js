5fbcfa5b547cfa87665446e5bf7fcd4be2d4dad2
rafaela <rafaelasilva110493@gmail.com>
2025-04-28 21:21:25 -0300

    testes unitários realizados.

--- src/index.js
+++ src/index.js
@@ -1,24 +1,5 @@
-const array = [1,2,1,2,2]
-let numAnterior = 0
-let moda = "Nao existe"
+const calcularModa = require("./calcular-moda");
 
-for (let n = 0; n < array.length; n++) {
-    let numvezes = 0
-    const numero = array[n]; 
-    
-    for (let i = 0; i < array.length; i++) {
-        const num = array[i]
-        if (numero === num) {
-            numvezes++
-        }
-    }
+const array = [1,2,1,2,2];
 
-    if (numvezes > numAnterior ) {
-        numAnterior = numvezes
-        moda = numero
-    } else if (numvezes === numAnterior && moda !== numero) {
-        moda = "Nao existe"
-    }
-}
-
-console.log("moda", moda)
+console.log(calcularModa(array));
